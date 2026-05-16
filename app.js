c078d09bf392b1ac1466e74aebb8fd1173f19c7e
Amit Gupta <amitgupta4263@gmail.com>
2026-05-16 14:10:27 +0530

    Initialize Express application with MongoDB connection and basic routing

---
+++ app.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const app = express();
+const mongooes = require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/test";
+
+main()
+  .then(() => {
+    console.log("Conneected to DB");
+  })
+
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongooes.connect(MONGO_URL);
+}
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+  res.send("Heyy I am Amit from india");
+});
+
+// index Route
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
+});
+
+//  new Route
+app.get("/listings/new", (req, res) =>{
+    res.render("listings/new.ejs");
+});
+
+// show Route
+app.get("/listings/:id", async(req, res) =>{
+    let{id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs",{ listing});
+});
+
+// Create Route
+app.post("/listings", async (req, res) => {
+    // let {title, discription, image, price, country, location} = req.body;   this is are first method
+
+    let listing = req.body.listing;
+    console.log(listing);
+
+})
+
+// app.get("/testListing", async (req, res) => {
+//   let sampleListing = new Listing({
+//     title: "My New Villa",
+//     discription: "By the beach",
+//     price: 12000,
+//     location: "Calangute, Goa",
+//     country: "India,",
+//   });
+//   await sampleListing.save();
+//   console.log("sample is saved");
+//   res.send("successfull testing");
+// });
+
+app.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
