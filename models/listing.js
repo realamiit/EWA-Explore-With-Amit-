650fd0e6d632e4136052a3c7fb24de6c87da9551
Amit Gupta <amitgupta4263@gmail.com>
2026-05-16 14:23:30 +0530

    Initial commit

---
+++ models/listing.js
@@ -0,0 +1,30 @@
+const { NumberUtils, Double, Decimal128 } = require("bson");
+const { builtinModules } = require("module");
+const mongooes = require("mongoose");
+const Schema = mongooes.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  discription: String,
+  image: {
+    type: String,
+    default:
+      "https://unsplash.com/photos/a-house-with-a-car-parked-in-front-of-it-4LLHJHyXQVk",
+    set: (v) =>
+      v === ""
+        ? "https://unsplash.com/photos/a-house-with-a-car-parked-in-front-of-it-4LLHJHyXQVk"
+        : v,
+  },
+  price: Number,
+  location: String,
+  country: String,
+  type: String,
+  guests: Number,
+  bedrooms: Number,
+});
+
+const listing = mongooes.model("Listing", listingSchema);
+module.exports = listing;
